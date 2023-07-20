371d2ccba4a054fe180cb2bff199313fcf378a4f
Harshal Patil <harshalspatil21@gmail.com>
2023-07-20 15:28:57 +0530

    No message

---
+++ com/Arrays/Main.java
@@ -0,0 +1,8 @@
+package com.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr={5,6,1,2,3,4};
+        System.out.println(ArrayRotateCheck.arrayRotateCheck(arr));
+    }
+}
