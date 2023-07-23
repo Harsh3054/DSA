2c95f6b699df1cee4be7c208563a409bb6744ea7
Harshal Patil <129618573+Harsh3054@users.noreply.github.com>
2023-07-23 17:05:43 +0530

    Delete com/Arrays directory

--- com/Arrays/Main.java
+++
@@ -1,8 +0,0 @@
-package com.Arrays;
-
-public class Main {
-    public static void main(String[] args) {
-        int[] arr={5,6,1,2,3,4};
-        System.out.println(ArrayRotateCheck.arrayRotateCheck(arr));
-    }
-}
